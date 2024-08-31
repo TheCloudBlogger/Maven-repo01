6b66bc0d4a9df62c7215d76a4ec5d0ca455b924e
Ujjawal <mail.cloudblogger@gmail.com>
2024-08-31 08:17:52 +0530

    firts-maven-project

---
+++ src/main/java/org/devops_company/first_maven_project/App.java
@@ -0,0 +1,13 @@
+package org.devops_company.first_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
